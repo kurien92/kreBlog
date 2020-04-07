67b9795c1995c23bbd3261e715b40af401e86bfb
kurien <kurien92@gmail.com>
2020-04-08 02:10:31 +0900

    스프링 5.0.8 버전 보안취약점 이슈로 인한 hotfix

--- target/test-classes/mybatis/sql/category-sql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `kreCategory` (
-  `categoryNo` int unsigned NOT NULL AUTO_INCREMENT,
-  `categoryParentNo` int DEFAULT NULL,
-  `categoryDepth` int NOT NULL,
-  `categoryOrder` int NOT NULL,
-  `categoryId` varchar(30) NOT NULL UNIQUE,
-  `categoryName` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`categoryNo`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
