27b35300d3d953d2ac2d785ed5ce1f8da063d3c2
kurien <kurien92@gmail.com>
2020-04-12 01:23:53 +0900

    서버 시간이 UTC 표준시간으로 설정되는 문제로 인하여 서버시작 시 한국시간(UTC+9)으로 설정하도록 변경

--- src/main/java/net/kurien/blog/listener/StartupApplicationListener.java
+++ src/main/java/net/kurien/blog/listener/StartupApplicationListener.java
@@ -3,6 +3,7 @@ package net.kurien.blog.listener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 import javax.inject.Inject;
 
@@ -26,17 +27,25 @@ public class StartupApplicationListener implements ApplicationListener<ContextRe
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		// TODO Auto-generated method stub
+		
+		// 서버 시간은 한국 시간으로 설정한다.
+		// 도커에서 Tomcat을 돌리는 경우 실제서버 설정과 관계 없이 UTC 표준시간으로 설정됨.
+		logger.info("user Timezone setup - Asia/Seoul");
+		//TimeZone을 null로 설정해야 캐시된 데이터가 삭제되어 user.timezone 설정이 적용된다.
+		TimeZone.setDefault(null); 
+		System.setProperty("user.timezone", "Asia/Seoul");
+		
 		Date date = Calendar.getInstance().getTime();
 		SimpleDateFormat sdf = new SimpleDateFormat("[yyyy/MM/dd HH:mm:ss.SSS]");
 		String formattedDate = sdf.format(date);
-		
-		logger.info(formattedDate + " Startup Application");
+
+		logger.info("[" + formattedDate + "] Startup Application");
 		
 		// 설정 검증 시작
-		logger.info(formattedDate + " Check setup start");
+		logger.info("Check setup start");
 		
 		try {
-			logger.info(formattedDate + " Check setup - IPv4");
+			logger.info("Check setup - IPv4");
 			
 			//IPv4로 설정한다.
 			System.setProperty("java.net.preferIPv4Stack", "true");
@@ -45,10 +54,10 @@ public class StartupApplicationListener implements ApplicationListener<ContextRe
 			iPAddressValidation.checkIPv4Setup();
 		} catch (NotSetupIPv4Exception e) {
 			// TODO Auto-generated catch block
-			logger.error(formattedDate + " " + e.getMessage());
+			logger.error("" + e.getMessage());
 		} catch (NotUseIPv4Exception e) {
 			// TODO Auto-generated catch block
-			logger.error(formattedDate + " " + e.getMessage());
+			logger.error("" + e.getMessage());
 			try {
 				throw new Exception(e.getMessage());
 			} catch (Exception e1) {
@@ -57,11 +66,11 @@ public class StartupApplicationListener implements ApplicationListener<ContextRe
 			}
 		}
 
-		logger.info(formattedDate + " Check setup done");
+		logger.info("Check setup done");
 		
 		// 서버 구동 시 실행되어야 할 메서드 실행
-		logger.info(formattedDate + " Execute startup method start");
+		logger.info("Execute startup method start");
 		
-		logger.info(formattedDate + " Execute startup method done");
+		logger.info("Execute startup method done");
 	}
 }
