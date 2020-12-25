f428db84e629c3f1df406896ffa0ef456960e28c
kurien <kurien92@gmail.com>
2020-12-25 18:27:07 +0900

    Token이 Serializable이 구현되지 않아 발생하는 문제 수정

--- src/main/java/net/kurien/blog/vo/Token.java
+++ src/main/java/net/kurien/blog/vo/Token.java
@@ -1,11 +1,12 @@
 package net.kurien.blog.vo;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import lombok.Data;
 
 @Data
-public class Token {
+public class Token implements Serializable {
 	private String key;
 	private Date expirationTime;
 }
